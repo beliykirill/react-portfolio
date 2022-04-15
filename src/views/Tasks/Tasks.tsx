fb2530a72ac8ea8467998294c0b6bff99736de52
--replace-all <vasyapupkin01230123@mail.ru>
2022-04-15 16:16:37 +0300

    task layout

--- src/views/Tasks/Tasks.tsx
+++ src/views/Tasks/Tasks.tsx
@@ -1,5 +1,19 @@
+import style from "./tasks.module.scss";
+
 const Tasks = () => {
-  return <div>Tasks</div>;
-}
+  return (
+    <div className={style.container}>
+      <header className={style.header}>TODO LIST</header>
+      <main className={style.main}>
+          <div className={style.filter}>
+              <input className={style.input} />
+          </div>
+          <div className={style.content}>
+              <div className={style.exercise}></div>
+          </div>
+      </main>
+    </div>
+  );
+};
 
 export { Tasks };
