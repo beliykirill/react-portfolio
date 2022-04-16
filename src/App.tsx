fc24eb7efda3eff44d475954b1149e1d492b3d0c
--replace-all <vasyapupkin01230123@mail.ru>
2022-04-16 11:50:36 +0300

    new layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import style from "./app.module.scss";
 import { useZoom } from "react-easy-hooks";
-import { Tasks } from "./views";
 
 const App = () => {
   const zoom = useZoom(0.9);
   return (
     <div style={zoom} className={style.container}>
-      <div className={style.sidebar}></div>
+      <div className={style.sidebar}>
+        <div className={style.profile}></div>
+        <ul className={style.navigation}>
+          <li className={style.category}></li>
+          <li className={style.category}></li>
+          <li className={style.category}></li>
+          <li className={style.category}></li>
+        </ul>
+      </div>
       <div className={style.wrapper}>
-        <Tasks />
+        
       </div>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
+import "modern-normalize";
+import "./assets/styles/styles.scss"
 import { setupStore } from "./redux/store";
 
 const root = ReactDOM.createRoot(
